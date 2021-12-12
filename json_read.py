f3eb219bb85bdc422e2d3121239fdcfa94ad8072
kinoshita0923 <sota0923ks15@gmail.com>
2021-12-13 00:27:49 +0900

    5501と5503だけなぜかうまくいかない

--- json_read.py
+++ json_read.py
@@ -2,12 +2,11 @@ import glob
 import json
 import csv
 
-# jsonフォルダのファイルを取得
-files = glob.glob("./json/IMG_5496*")
-
 # 右手のデータの要素数を取得
-length = len(json.load(open(files[0]))['people'][0]['hand_right_keypoints_2d'])
+length = 0
 data = []
+files = []
+path = ""
 
 # jsonデータをdataに保存
 def json_read(i):
@@ -18,12 +17,21 @@ def json_read(i):
 
 # データをcsvに書き込む
 def write_csv(i):
-    file_name = "./csv/json_data" + str(i) + ".csv"
+    file_name = "./csv/" + path + "/json_data" + str(i) + ".csv"
     with open(file_name, "w", newline = "") as f:
         csv.writer(f).writerows(data)
 
 # 要素数の個数と同じ数のcsvファイルを作成
-for i in range(length):
-    json_read(i)
-    write_csv(i+1)
-    data = []
+def create_csv():
+    for i in range(length):
+        json_read(i)
+        write_csv(i+1)
+        data = []
+
+# jsonフォルダのファイルを取得
+for i in range(5496, 5505, 1):
+    length = len(json.load(open("json\IMG_" + str(i) + "_000000000000_keypoints.json"))['people'][0]['hand_right_keypoints_2d'])
+    files = glob.glob("./json/IMG_" + str(i) + "*")
+    path = "IMG_" + str(i)
+    create_csv()
+    files = []
